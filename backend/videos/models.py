3f66a769a237ca341b45badfa2193d9f6ac0771e
defutfqr <gomboev13@bk.ru>
2025-05-18 19:34:06 +0700

    Update models.py

--- backend/videos/models.py
+++ backend/videos/models.py
@@ -1,1 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
 
+class Video(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    file = models.FileField(upload_to='videos/')
+    thumbnail = models.ImageField(upload_to='thumbnails/')
+    upload_date = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    views = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    video = models.ForeignKey(Video, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
