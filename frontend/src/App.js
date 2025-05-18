bd3f1b1d7b8ec841af81d20801756e3724e61ab8
defutfqr <gomboev13@bk.ru>
2025-05-18 20:05:46 +0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
+import VideoPage from './pages/VideoPage/VideoPage';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/video/:id" element={<VideoDetailPage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/video/:id" element={<VideoPage />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
+
+export default App;
